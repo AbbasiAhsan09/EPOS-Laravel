7e380a0e3519a6a47b34a98ec51d954d185e12c5
AhsanAbbasi09 <ahsansarim56@gmail.com>
2023-12-31 19:40:06 +0500

    DbBackupScript

--- app/Http/Controllers/DBBackupController.php
+++ app/Http/Controllers/DBBackupController.php
@@ -1,11 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use Carbon\Carbon;
 use Spatie\Dropbox\Client;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Http\Request;
+
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+use Spatie\Dropbox\TokenProvider;
+
 
 class DBBackupController extends Controller
 {
@@ -17,28 +24,51 @@ class DBBackupController extends Controller
        $this->folderPath = '/'.env('DROPBOX_BACKUP_PATH').'/'; 
     }
 
+
     public function DbBackup()
-    {
-        // Create a backup of the default database connection
-        $database = config('database.default');
-        $backupPath = storage_path('app/backups');
-        $filename = 'backup_' . date('Y-m-d_His') . '.sql';
+{
+    // Create a backup of the default database connection
+    $database = config('database.default');
+    $backupPath = storage_path('app/backups');
+    $filename = 'backup_' . date('Y-m-d_His') . '.sql';
+    
+    $descriptorSpec = [
+        0 => ['pipe', 'r'], // stdin
+        1 => ['pipe', 'w'], // stdout
+        2 => ['pipe', 'w'], // stderr
+    ];
+    
+    $command = sprintf(
+        '"C:\\laragon\\bin\\mysql\\mysql-5.7.33-winx64\\bin\\mysqldump" --user=%s --password=%s --host=%s %s',
+        escapeshellarg(config("database.connections.{$database}.username")),
+        escapeshellarg(config("database.connections.{$database}.password")),
+        escapeshellarg(config("database.connections.{$database}.host")),
+        escapeshellarg(config("database.connections.{$database}.database"))
+    );
 
-        $command = sprintf(
-            'mysqldump --user=%s --password=%s --host=%s %s > %s',
-            config("database.connections.{$database}.username"),
-            config("database.connections.{$database}.password"),
-            config("database.connections.{$database}.host"),
-            config("database.connections.{$database}.database"),
-            "{$backupPath}/{$filename}"
-        );
+    $process = proc_open($command, $descriptorSpec, $pipes, $backupPath);
 
-        exec($command);
+    if (is_resource($process)) {
+        fclose($pipes[0]); // Close stdin as we're not writing any input
+        
+        $backupFile = fopen("{$backupPath}/{$filename}", 'w');
+        stream_copy_to_stream($pipes[1], $backupFile); // Read stdout and write to backup file
+        fclose($pipes[1]);
+        
+        $errors = stream_get_contents($pipes[2]); // Read stderr
+        
+        fclose($backupFile);
+        proc_close($process);
+        
+        // if (!empty($errors)) {
+        //     dump($errors);
+        //     // Handle errors if needed
+        //     return 'Backup creation failed!';
+        // }
 
         // Optional: If you want to gzip the backup file
-        exec("gzip {$backupPath}/{$filename}");
-        $files = \File::files($backupPath);
-
+        $gzipProcess = new Process(['gzip', "{$backupPath}/{$filename}"]);
+        $gzipProcess->run();
 
         // Optional: Clean up older backups if needed
         $this->cleanUpOldBackups($backupPath);
@@ -47,10 +77,14 @@ class DBBackupController extends Controller
 
         return 'Backup created and uploaded!';
     }
+
+    return 'Failed to start backup process!';
+}
+
     private function cleanUpOldBackups($backupPath)
     {
         $files = \File::files($backupPath);
-        $maxBackups = 2;
+        $maxBackups = 3;
 
         if (count($files) > $maxBackups) {
             $sortedFiles = collect($files)->sortByDesc(function ($file) {
@@ -95,7 +129,8 @@ class DBBackupController extends Controller
     // $file->storeAs('uploads', $fileName);
 
     // Get an instance of the Dropbox client
-    $dropboxClient = new Client(config('services.dropbox.token'));
+    $tokenGet = $this->getToken(config('services.dropbox.app_key'),config('services.dropbox.secret'), config('services.dropbox.refresh_token'));
+    $dropboxClient = new Client($tokenGet);
     $this->clearDropBoxFolder($this->folderPath);
     // Upload the file to Dropbox
     $dropboxClient->upload(
@@ -115,18 +150,56 @@ class DBBackupController extends Controller
 
 public function clearDropBoxFolder($path)
 {
-   // Instantiate the Dropbox client
-$dropboxClient = new Client(config('services.dropbox.token'));
+    // Instantiate the Dropbox client
+    $tokenGet = $this->getToken(config('services.dropbox.app_key'),config('services.dropbox.secret'), config('services.dropbox.refresh_token'));
+    $dropboxClient = new Client($tokenGet);
 
-// Specify the folder path to delete files from
-$folderPath = $this->folderPath;
+    // Specify the folder path to delete files from
+    $folderPath = $this->folderPath;
 
-// Get a list of all files in the folder
-$files = $dropboxClient->listFolder($folderPath);
-// dd($files["entries"] );
-// Loop through the files and delete them
-foreach ($files["entries"] as $file) {
-    $dropboxClient->delete($file['path_display']);
+    // Get a list of all files in the folder
+    $files = $dropboxClient->listFolder($folderPath);
+
+    // Sort files by modification time (you might adjust this based on your criteria)
+    usort($files['entries'], function ($a, $b) {
+        return strtotime($a['client_modified']) - strtotime($b['client_modified']);
+    });
+
+    // Keep the last file in the array (to exclude it from deletion)
+    $lastFile = array_pop($files['entries']);
+
+    // Loop through the files (except the last one) and delete them
+    foreach ($files['entries'] as $file) {
+        $dropboxClient->delete($file['path_display']);
+    }
+    
+    // Optionally, return information about the last file
+    return $lastFile;
 }
+
+public function getToken($key, $secret, $refreshToken)
+{
+    try {
+        $client = new \GuzzleHttp\Client();
+        $res = $client->request("POST", "https://{$key}:{$secret}@api.dropbox.com/oauth2/token", [
+            'form_params' => [
+                'grant_type' => 'refresh_token',
+                'refresh_token' => $refreshToken,
+            ]
+        ]);
+        
+        if ($res->getStatusCode() == 200) {
+            return json_decode($res->getBody(), TRUE)['access_token'];
+        } else {
+            info(json_decode($res->getBody(), TRUE));
+            return false;
+        }
+    } catch (\Exception $e) {
+        return $e;
+        return false;
+    }
+
+
 }
+
 }
