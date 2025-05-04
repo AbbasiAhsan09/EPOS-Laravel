84d486ee4c9128c372c10eae7704499ddbfff01a
Ahsan <ahsansarim56@gmail.com>
2025-05-04 22:29:20 +0500

    JV Detail PDF Columns set

--- resources/views/journal-vouchers/pdf/detail.blade.php
+++ resources/views/journal-vouchers/pdf/detail.blade.php
@@ -13,8 +13,8 @@
         <th width="100px">Reference No.</th>
         <th>Account</th>
         <th>Description</th>
-        <th>Credit</th>
         <th>Debit</th>
+        <th>Credit</th>
         <th>Mode</th>
     </thead>
     <tbody>
@@ -24,8 +24,8 @@
             <td>{{$item->reference_no}}</td>
             <td>{{$item->account->title}}</td>
             <td>{{$item->description}}</td>
-            <td>{{$item->credit ? ConfigHelper::getStoreConfig()["symbol"].number_format($item->credit,2) : '-'}}</td>
             <td>{{$item->debit ? ConfigHelper::getStoreConfig()["symbol"].number_format($item->debit,2) : '-'}}</td>
+            <td>{{$item->credit ? ConfigHelper::getStoreConfig()["symbol"].number_format($item->credit,2) : '-'}}</td>
             <td>{{$item->mode ? ucfirst($item->mode) : '-'}}</td>
         </tr>
         @endforeach
@@ -34,8 +34,8 @@
     </tbody>
     <tr>
         <td colspan="3">Total : ({{$voucher->entries->count()}})</td>
-        <td>{{ConfigHelper::getStoreConfig()["symbol"].number_format($voucher->entries->sum("credit"),2)}}</td>
         <td>{{ConfigHelper::getStoreConfig()["symbol"].number_format($voucher->entries->sum("debit"),2)}}</td>
+        <td>{{ConfigHelper::getStoreConfig()["symbol"].number_format($voucher->entries->sum("credit"),2)}}</td>
         <td></td>
     </tr>
 </table>
